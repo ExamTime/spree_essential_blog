2ceb06a23a982544cec4b6b9acdc77a2a91402ce
Anthony Batchelor <anthony.batchelor@examtime.com>
2012-12-20 14:00:50 +0000

    Improve validation of page parameter for blog posts

--- app/controllers/spree/blogs/posts_controller.rb
+++ app/controllers/spree/blogs/posts_controller.rb
@@ -3,16 +3,16 @@ class Spree::Blogs::PostsController < Spree::BaseController
   include SpreeEssentialBlog::PostsControllerHelper
 
   helper "spree/products"
-  
+
   before_filter :get_sidebar, :only => [:index, :search, :show]
-  
+
   def index
     @posts_by_month = default_scope.limit(50).group_by { |post| post.posted_at.strftime("%B %Y") }
     scope = default_scope
     if params[:year].present?
       year  = params[:year].to_i
       month = 1
-      day   = 1  
+      day   = 1
       if has_month = params[:month].present?
         if has_day = params[:day].present?
           day  = params[:day].to_i
@@ -26,26 +26,39 @@ class Spree::Blogs::PostsController < Spree::BaseController
         if has_day
           stop = start + 1.day
         end
-      end    
+      end
       scope = scope.where("posted_at >= ? AND posted_at <= ?", start, stop)
     end
-    @posts = scope.page(params[:page]).per(Spree::Post.per_page)
+    page_id = get_safe_page_id(params)
+    @posts = scope.page(page_id).per(Spree::Post.per_page)
   end
-  
+
   def search
-		query = params[:query].gsub(/%46/, '.')	
-		@posts = default_scope.tagged_with(query).page(params[:page]).per(Spree::Post.per_page)
-		get_tags		
-		render :template => 'spree/blogs/posts/index'
-	end
-	
+    query = params[:query].gsub(/%46/, '.')
+    page_id = get_safe_page_id(params)
+    @posts = default_scope.tagged_with(query).page(page_id).per(Spree::Post.per_page)
+    get_tags
+    render :template => 'spree/blogs/posts/index'
+  end
+
   def show
-    @post = default_scope.includes(:tags, :images, :products).find_by_path(params[:id]) rescue nil
+    @post = default_scope.includes(:tags, :images, :products).find_by_path(params[:id].to_i) rescue nil
     return redirect_to archive_posts_path unless @post
   end
-  
-	def archive
-		@posts = default_scope.all
+
+  def archive
+    @posts = default_scope.all
   end
-  
+
+private
+
+  # Avoid SQL injections etc by sending you to page
+  # 1 if the page number doesn't look like a number
+  def get_safe_page_id(params)
+    return 1 unless params.present?
+    page = params.fetch(:page, nil)
+    id = page.to_i # Weird strings become 0
+    (id == 0) ? 1 : id
+  end
+
 end
